3fdf1873426ab3209eeca79b0ce1e164cff1e0b4
Ubuntu <rajdebna83@main-vm-ubuntu.ltpz2p25vbvudffm25vmwkndma.rx.internal.cloudapp.net>
2021-11-21 06:59:23 +0000

    added TaskClient endpoints

--- polyglot-clients/node-typescript-grpc/test/index.ts
+++ polyglot-clients/node-typescript-grpc/test/index.ts
@@ -1,15 +1,15 @@
-import { iEqual, TaskClient, MetadataClient } from 'netflix-conductor-ts-grpc';
+import { iEqual, TaskClient, MetadataClient, WorkflowClient } from 'netflix-conductor-ts-grpc';
 
 console.log(iEqual(13));
 
-// const service = new TaskClient("localhost:8090");
-// service.GetQueueAllInfo((err,resp) => {
-//     console.log(err);
-//     console.log(resp);
-// })
-
-const service = new MetadataClient("localhost:8090");
-service.CreateWorkflow("kitchensink", (err,resp) => {
+const service = new TaskClient("localhost:8090");
+service.GetQueueAllInfo((err,resp) => {
     console.log(err);
     console.log(resp);
 })
+
+// const service = new WorkflowClient("localhost:8090");
+// service.StartWorkflow("kitchensink", (err,resp) => {
+//     console.log(err);
+//     console.log(resp);
+// })
