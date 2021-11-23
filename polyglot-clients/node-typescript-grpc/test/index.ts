3afb01b4d307a06b4f29db10983f19615fb44643
Ubuntu <rajdebna83@main-vm-ubuntu.ltpz2p25vbvudffm25vmwkndma.rx.internal.cloudapp.net>
2021-11-23 15:30:24 +0000

    added code for Execute

--- polyglot-clients/node-typescript-grpc/test/index.ts
+++ polyglot-clients/node-typescript-grpc/test/index.ts
@@ -1,12 +1,19 @@
-import { iEqual, TaskClient, MetadataClient, WorkflowClient } from 'netflix-conductor-ts-grpc';
+import { iEqual, TaskClient, WorkflowClient
+    , ConductorWorker, TaskReturnObject } from 'netflix-conductor-ts-grpc';
 
-console.log(iEqual(13));
+const exec_function = () => ({'status': 'COMPLETED', 'output': {}, 'logs': []} as TaskReturnObject);
 
-const service = new TaskClient("localhost:8090");
-service.GetQueueAllInfo((err,resp) => {
-    console.log(err);
-    console.log(resp);
-})
+let cc = new ConductorWorker("localhost:8090", 2, 2000, "5e17f8cf-46dd-4c72-808c-edc4b6bd16db");
+
+cc.Start("task_1", exec_function);
+
+//console.log(iEqual(13));
+
+// const service = new TaskClient("localhost:8090");
+// service.GetQueueAllInfo((err,resp) => {
+//     console.log(err);
+//     console.log(resp);
+// })
 
 // const service = new WorkflowClient("localhost:8090");
 // service.StartWorkflow("kitchensink", (err,resp) => {
